a984fdc5fb9ff917c4be42a8461577a0a9f31c59
TeeJude <teejayjude@gmail.com>
2023-11-07 08:10:58 -0800

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the 
+number_of_subscribers
+"""
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries the Reddit API and returns the 
+    number of subscribers to the subreddit"""
+
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
